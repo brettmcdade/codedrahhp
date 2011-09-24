4faf7f1a3e8ca590b1f18b195056464a94e8044b
Brett <brettmcdade@gmail.com>
2011-09-23 23:10:22 -0500

    changes to layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,10 @@
 class UsersController < ApplicationController
 	
-  before_filter :authenticate_user!#, :except => [:index] 
+  before_filter :authenticate_user!
 
   def show
    @user = User.find_by_username(params[:id])
-   #user = User.find(params[:id])
-	#@title = @user.username
-  end
-  
-  def edit
-  
-    @title = "Edit Profile"
-	@user = current_user
-	
-	if param_posted?(:user)
-		if @user.update_attributes(params[:user])
-			flash[:notice] = "Changes saved"
-			redirect_to @user
-		else
-			flash[:notice] = "Changes were not saved"	
-		end
-  	end
+   @title = @user.username
   end
 
 end
--- app/helpers/digitalfiles_helper.rb
+++
@@ -1,2 +0,0 @@
-module DigitalfilesHelper
-end
